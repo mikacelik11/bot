801876750e22c990bbc9835de3c20f3e1e09c7e3
MikaC11-bit <mikael_celik@hotmail.com>
2026-04-09 14:37:31 -0700

    added more documentation

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ import random
 import math
 
 # -------------------------------------------------------
-# RULES
+# RULE
 # Each rule is made of a tuple (pattern, response)
 # Pattern is a regex string response is what the bot says
 # TODO: Add more rules as you build out the bot
